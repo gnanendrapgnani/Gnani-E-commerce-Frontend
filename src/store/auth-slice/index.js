88e251eea65d7cce114680b3740a509c9f023d8c
Gnanendra P <gnanendrapnani@gmail.com>
2025-08-04 03:37:52 +0530

    Auth is logic and implemation is Completed need integrate the logout api

--- src/store/auth-slice/index.js
+++ src/store/auth-slice/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const initialState = {
   isAuthenticated: false,
-  isLoading: false,
+  isLoading: true,
   user: null,
 };
 
@@ -18,6 +18,21 @@ export const registerUser = createAsyncThunk(
     return response.data;
   }
 );
+
+export const checkAuth = createAsyncThunk("/auth/check-auth", async () => {
+  const response = await axios.get(
+    "http://localhost:3000/api/auth/check-auth",
+    {
+      withCredentials: true,
+      headers: {
+        "Cache-Control":
+          "no-store, no-Cache, must-revalidate, proxy-revalidate",
+        // Expries: "0",
+      },
+    }
+  );
+  return response.data;
+});
 
 export const loginUser = createAsyncThunk("/auth/login", async (formData) => {
   const response = await axios.post(
@@ -58,6 +73,19 @@ const authSlice = createSlice({
         state.isAuthenticated = action.payload.success;
       })
       .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(checkAuth.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(checkAuth.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+      })
+      .addCase(checkAuth.rejected, (state, action) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
